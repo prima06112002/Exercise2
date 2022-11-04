8779d5d9fb28604c94736eb83c2e381ccd874b09
Prima06112002 <ramadhan06112002@gmail.com>
2022-11-04 16:08:37 +0700

    add get array elements

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -17,12 +17,34 @@ namespace Exercise2
         int n, i, j, val, flag;
         void input()
         {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n====================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n====================");
+
+            //get array elements
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+
+
 
 
 
 
         }
-     
     }
 }
 
